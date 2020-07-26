35c2b2d5df4bf7d8b6cc8d3b9a7b22329c6bb458
mdirshaddev <mdirshaddev@gmail.com>
2020-07-26 12:26:34 +0530

    Testing The deployment on github.io.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Md Irshad Portfolio
         </a>
       </header>
     </div>
