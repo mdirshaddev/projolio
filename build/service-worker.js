000d62c8eba1e01118279e1addfe3aba044f3507
mdirshaddev <mdirshaddev@gmail.com>
2020-07-27 16:03:58 +0530

    Adding some more changes.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c7c548e2844ef71cbf2e4aae426a3e05.js"
+  "/portfolio/precache-manifest.663c31349bbfca05ac2888890b397140.js"
 );
 
 self.addEventListener('message', (event) => {
