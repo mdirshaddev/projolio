417717645c09270bc87a15c283d07a46222cf46c
mdirshaddev <mdirshaddev@gmail.com>
2020-07-28 12:11:14 +0530

    moving to mobile responsive content.

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
 export default class CardInfo extends React.Component{
     render(){
         return(
-            <div className="container">
+          <div className="container">
+            <div className="content">
+              <div className="img-side">
+                <img src={i} className="blue-box"></img>
+              </div>
+              <div className="info-side">
+                <div className="intro">
+                  Hello, my name is
+                </div>
+                <h1 className="fname">Md Irshad</h1>
+                <div className="text-info">
+                  <p>I'm a full-stack developer specialised in frontend and backend development for complex scalable web apps. I write about web development on <a href="#">my blog</a> and regularly <a href="#">speak</a> at various web conferences and meetups. Want to know how I may help your project? Check out my project <a href="#">case studies</a> and <a href="#">resume.</a></p>
+                </div>
+                <a href="#" className="hire-btn">Hire Me</a>
+              </div>
             </div>
+          </div>
         )
     }
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import i from '../images/table.jpeg';
 export default class Navbar extends React.Component{
     render(){
         return(
@@ -8,23 +8,24 @@ export default class Navbar extends React.Component{
               <div className="holdings">
                 <div className="sociallinks">
                   <ul>
-                    <i className="fa fa-github"></i>
-                    <i className="fa fa-twitter"></i>
-                    <i className="fa fa-facebook"></i>
-                    <i className="fa fa-dribbble"></i>
-                    <i className="fa fa-instagram"></i>
+                    <a href="#"><i className="fa fa-github"></i></a>
+                    <a href="#"><i className="fa fa-twitter"></i></a>
+                    <a href="#"><i className="fa fa-facebook"></i></a>
+                    <a href="#"><i className="fa fa-dribbble"></i></a>
+                    <a href="#"><i className="fa fa-instagram"></i></a>
                   </ul>
                 </div>
                 <div className="routes">
                   <nav>
                     <ul>
-                      <li><a href="#">About</a></li>
+                      <li><a href="/about" className="active">About</a></li>
                       <li><a href="#">Projects</a></li>
                       <li><a href="#">Resume</a></li>
+                      <li><a href="#">Talks</a></li>
                       <li><a href="#">Blog</a></li>
                       <li><a href="#">Contact</a></li>
                       <li>
-                        <a href="">Know More</a>
+                        <a href="">Pages</a>
                         <div className="drop-menu">
                           <a href="#">Project Case Studies</a>
                           <a href="#">Blogs</a>
@@ -33,6 +34,9 @@ export default class Navbar extends React.Component{
                       </li>
                     </ul>
                   </nav>
+                </div>
+                <div className="show-menu-btn">
+                  <i className="fa fa-bars"></i>
                 </div>
               </div>
             </div>
