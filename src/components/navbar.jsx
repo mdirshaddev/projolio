8c3f05e584edddf57f697f76878ddddeabaf8b9f
mdirshaddev <mdirshaddev@gmail.com>
2020-07-29 09:40:45 +0530

    Mobile responsive header.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default class Navbar extends React.Component{
     render(){
@@ -8,35 +7,41 @@ export default class Navbar extends React.Component{
               <div className="holdings">
                 <div className="sociallinks">
                   <ul>
-                    <a href="#"><i className="fa fa-github"></i></a>
-                    <a href="#"><i className="fa fa-twitter"></i></a>
-                    <a href="#"><i className="fa fa-facebook"></i></a>
-                    <a href="#"><i className="fa fa-dribbble"></i></a>
-                    <a href="#"><i className="fa fa-instagram"></i></a>
+                    <a href="#github"><i className="fa fa-github"></i></a>
+                    <a href="#twitter"><i className="fa fa-twitter"></i></a>
+                    <a href="#facebook"><i className="fa fa-facebook"></i></a>
+                    <a href="#dribble"><i className="fa fa-dribbble"></i></a>
+                    <a href="#instagram"><i className="fa fa-instagram"></i></a>
                   </ul>
                 </div>
                 <div className="routes">
                   <nav>
                     <ul>
                       <li><a href="/about" className="active">About</a></li>
-                      <li><a href="#">Projects</a></li>
-                      <li><a href="#">Resume</a></li>
-                      <li><a href="#">Talks</a></li>
-                      <li><a href="#">Blog</a></li>
-                      <li><a href="#">Contact</a></li>
+                      <li><a href="#projects">Projects</a></li>
+                      <li><a href="#resume">Resume</a></li>
+                      <li><a href="#talks">Talks</a></li>
+                      <li><a href="#blog">Blog</a></li>
+                      <li><a href="#contact">Contact</a></li>
                       <li>
-                        <a href="">Pages</a>
+                        <a href="#pages">Pages</a>
                         <div className="drop-menu">
-                          <a href="#">Project Case Studies</a>
-                          <a href="#">Blogs</a>
-                          <a href="#">Contact Form</a>
+                          <a href="#project">Project Case Studies</a>
+                          <a href="#blogs">Blogs</a>
+                          <a href="#contact">Contact Form</a>
                         </div>
                       </li>
                     </ul>
                   </nav>
                 </div>
                 <div className="show-menu-btn">
-                  <i className="fa fa-bars"></i>
+                  <input type="checkbox" id="chk"/>
+                  <label htmlFor="chk" className="show-btn">
+                    <i className="fa fa-bars">
+                  </i></label>
+                  <label htmlFor="chk" className="hide-btn">
+                    <i className="fa fa-times">
+                  </i></label>
                 </div>
               </div>
             </div>
